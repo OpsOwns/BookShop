d4c783f6d969e3bd77c3ae25e796c532d8a2ca85
Ops <krzysztof.papciak@hotmail.com>
2021-01-09 19:53:16 +0100

    Refactor add identity db

---
+++ src/Shop.Auth/Shop.Auth.API/Shop.Auth.API/Services/DbInitializerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Shop.Auth.Infrastructure.Context;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shop.Auth.API.Services
+{
+    public class DbInitializerService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public DbInitializerService(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _serviceProvider.GetRequiredService<IConfiguration>();
+            using var scope = _serviceProvider.CreateScope();
+            await scope.ServiceProvider.GetRequiredService<IdentityAccountContext>().Database.EnsureCreatedAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
---
+++ src/Shop.Shared/Shop.Shared/Model/DatabaseOption.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Shared.Model
+{
+    public class DatabaseOption
+    {
+        public string ConnectionString { get; set; }
+        public bool InMemmory { get; set; }
+    }
+}
