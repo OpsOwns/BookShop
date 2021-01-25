a3aa80b17c38f5275d9eabcb5481c208e50e483e
Ops <krzysztof.papciak@hotmail.com>
2021-01-25 19:38:12 +0100

    Add shop book api

---
+++ src/Shop.Shared/Shop.Shared/Model/AuthOption.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.Shared.Model
+{
+    public class AuthOption
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
